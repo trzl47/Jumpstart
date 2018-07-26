54ef3a603ced95a41d9986727c8608ca3b33353b
trzl <brownty47@gmail.com>
2018-07-26 05:09:01 +0000

    Added Help component

--- services/react/src/app/HomePage.jsx
+++ services/react/src/app/HomePage.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header.jsx';
 import Jumbotron from './components/Jumbotron.jsx';
 import Feature from './components/Feature.jsx';
 import Projects from './components/Projects.jsx';
+import Help from './components/Help.jsx';
 // styles
 // import '../css/homepage.css';
 
@@ -15,14 +16,7 @@ const HomePage = () => {
 			<Jumbotron />
 			<Feature />
 			<Projects />
-
-
-		<div className='container-fluid help'>
-			<strong>Having trouble thinking of an idea?</strong>
-			<p>Inventors' block strikes at the worst times. But that's all changed with Jumpstart's Good Idea Generator Lite (GIGLi).</p>
-			<a href='/#' className='btn btn-primary btn-lg'>Learn more</a>
-		</div>
-
+			<Help />
 		</section>
 	);
 };
---
+++ services/react/src/app/components/Help.jsx
@@ -0,0 +1,16 @@
+// libs
+import React from 'react';
+// styles
+import '../../css/help.css';
+
+const Help = () => {
+	return (
+		<div className='container-fluid help'>
+			<strong>Having trouble thinking of an idea?</strong>
+			<p>Inventors' block strikes at the worst times. But that's all changed with Jumpstart's Good Idea Generator Lite (GIGLi).</p>
+			<a href='/#' className='btn btn-primary btn-lg'>Learn more</a>
+		</div>
+	);
+};
+
+export default Help;
