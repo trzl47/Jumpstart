500d10ddb51e2cf1c9271e7d468b9cd851046c53
trzl <brownty47@gmail.com>
2018-08-02 20:08:19 +0000

    Fixed deployment

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-docker-compose -f build.yml up --build -d && \
+docker-compose -f docker-compose-build.yml up --build -d && \
 docker system prune --volumes && \
-mkdir services/react-nginx/build && \
-cp -r services/react/build/client services/react-nginx/build && \
-docker build -t trzl/portfolio:jumpstart-proxy.1 -f ./services/reverseproxy/Dockerfile ./services/reverseproxy && \
-docker push trzl/portfolio:jumpstart-proxy.1 && \
-docker build -t trzl/portfolio:jumpstart-react.1 -f ./services/react-nginx/build.Dockerfile ./services/react-nginx && \
-docker push trzl/portfolio:jumpstart-react.1 && \
+mkdir services/jump-nginx/build && \
+cp -r services/react/build/client services/jump-nginx/build && \
+docker build -t trzl/portfolio:jump-react.1 -f ./services/jump-nginx/build.Dockerfile ./services/jump-nginx && \
+# docker push trzl/portfolio:jump-react.1 && \
+docker build -t trzl/portfolio:jump-proxy.1 -f ./services/reverseproxy/Dockerfile ./services/reverseproxy && \
+# docker push trzl/portfolio:jump-proxy.1 && \
 docker-compose down && \
 docker system prune --volumes && \
-sudo rm -rf services/react-nginx/build && \
+sudo rm -rf services/jump-nginx/build && \
 sudo rm -rf services/react/build/client && \
 echo "Build complete!"
